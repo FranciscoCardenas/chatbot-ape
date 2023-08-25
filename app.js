a4fcb0c18137ec7a9b264ab1b6e8cd372e1c5b61
Francisco Emanuel Cardenas Ramos <franciscocardenasra@gmail.com>
2023-08-25 09:56:54 -0600

    Agregar FEchas a contratos y contactos de ape

--- app.js
+++ app.js
@@ -70,10 +70,13 @@ const InformacionContrato = addKeyword(['2']).addAnswer(
     flowDynamic([{body:'No se encontraron datos del numero de cliente *'+ ctx.body+'* . Algo mas en lo que podemos ayudarte?', buttons: [{ body: 'SI, POR FAVOR' }, { body: 'NO, GRACIAS' } ]}])
    }else{ 
 
-
-    for(i=0;i<data.items.length;i++){ 
+//console.log(data.items[0]);
+    for(i=0;i<data.items.length;i++){
       
-       flowDynamic([{body:data.items[i]['numero de contrato']+' - '+data.items[i].suma}])
+       
+       flowDynamic([{body: 'Contrato : *'+data.items[i]['numero de contrato']+'* Saldo *'+data.items[i].suma.split(/\s+/).join('')+
+       '* \nTermino Vigencia: *'+data.items[i].vigencia_final.split(/\s+/).join(' ')+
+       '*  \nRenovación: *'+data.items[i].vigencia_inicial.split(/\s+/).join(' ')+'*' }])
       
        if (i === data.items.length - 1) {
         setTimeout(() => {
@@ -118,7 +121,7 @@ const SaldoContrato = addKeyword(['3']).addAnswer(
         fetch(apiUrl, requestOptions)
           .then(response => response.json())
           .then(data => {
-           
+           console.log(data.items[0])
        if(data.items.length==0){
         flowDynamic([{body:'No se encontraron datos del contrato *'+ NUMERO_CONTRATO+'* . Algo mas en lo que podemos ayudarte?', buttons: [{ body: 'SI, POR FAVOR' }, { body: 'NO, GRACIAS' } ]}],null,null,[RespuestaNO])
        }else{ 
@@ -161,7 +164,7 @@ const PagarContrato = addKeyword(['4']).addAnswer(
         fetch(apiUrl, requestOptions)
           .then(response => response.json())
           .then(data => {
-            console.log(data.items[0].cliente_id)
+            //console.log(data.items[0].cliente_id)
        if(data.items.length==0){
         flowDynamic([{
          body:'No se encontraron datos del contrato *'+ NUMERO_CONTRATO+'* . Algo mas en lo que podemos ayudarte?', buttons: [{ body: 'SI, POR FAVOR' }, { body: 'NO, GRACIAS' } ]}],null,null,[RespuestaNO])
@@ -193,31 +196,83 @@ const PagarContrato = addKeyword(['4']).addAnswer(
     [pagar]
 )
 
+//OPCION 5 
+const contactoplaza = addKeyword(['5']).addAnswer(
+  [
+      'Por favor Ingresa tu numero de contrato',
+  ],
+  {capture:true},
+  ((ctx,{flowDynamic,fallBack}) =>{
+      const NUMERO_CONTRATO =String(ctx.body).toUpperCase();  
+      const apiUrl = 'https://nuevo.apexdeape.com.mx/ords/apeppdb1/xxschema_contratos/ar1/v1/contacto_plaza';
+      // Configurar los headers
+      const headers = new Headers();
+      headers.append('NUMERO_CONTRATO', NUMERO_CONTRATO);
+      // Configurar la solicitud
+      const requestOptions = {
+        method: 'GET',
+        headers: headers,
+      };
+      // Hacer la solicitud
+      fetch(apiUrl, requestOptions)
+        .then(response => response.json())
+        .then(data => {
+         console.log(data.items[0])
+     if(data.items.length==0){
+      flowDynamic([{body:'No se encontraron datos del contrato *'+ NUMERO_CONTRATO+'* . Algo mas en lo que podemos ayudarte?', buttons: [{ body: 'SI, POR FAVOR' }, { body: 'NO, GRACIAS' } ]}],null,null,[RespuestaNO])
+     }else{ 
+      flowDynamic([{body:'Para aclacion de dudas comunicate con '+data.items[0].nombre+' al numero '+data.items[0].numero_telefono+' o un correo a la siguiente direccion *'+data.items[0].correo +'*, ¿Algo mas en lo que podemos ayudarte?', buttons: [{ body: 'SI, POR FAVOR' }, { body: 'NO, GRACIAS' }]}],null,null,[RespuestaNO])
+          }
+
+        })
+        .catch(error => {
+        console.log(error.name)
+        if(error.name='SyntaxError'){
+          flowDynamic([{body:'No se encontraron datos del contrato '+NUMERO_CONTRATO+',¿Algo mas en lo que podemos ayudarte?', buttons: [{ body: 'SI, POR FAVOR',  }, { body: 'NO, GRACIAS' } ],}],null,null,[RespuestaNO])
+        }
+        });
+      }), 
+  
+)
+
+
 const flowPrincipal = addKeyword(['Hola','SI, POR FAVOR','Buen dia','Buenos dias','buenos días','buenas noches','buenas tardes'])
    
     .addAnswer(
         [
-            'Buen dia , este es nuestro menu de opciones, enviano el numero para darte seguimiento',
+            '¡Buen día! Aquí tienes nuestro menú de opciones, Envía el número de la opción que deseas consultar. ',
             '👉 *1* Conocer tu numero de cliente',
             '👉 *2* Consulta la informacion de tus contratos',
             '👉 *3* Consultar el Saldo de tu contrato',
             '👉 *4* Pagar contrato',
+            '👉 *5* Hablar con un asesor',
+            
         ],{capture:true},
       null 
       ,
-        [NumeroCliente, InformacionContrato, SaldoContrato, PagarContrato]
+        [NumeroCliente, InformacionContrato, SaldoContrato, PagarContrato,contactoplaza]
     ) 
 
 const main = async () => {
     const adapterDB = new JsonFileAdapter()
     const adapterFlow = createFlow([flowPrincipal])
-
+/*
+    //Produccion
     const adapterProvider = createProvider(MetaProvider, {
         jwtToken: 'EAAVZCOTzwHAcBO5LjxXJhaQhTiUL3mT56wJo5vsYAmVrgpyeXDC2ooicdJH8iZBGhHU7pHj5Jzg5pqOr3UuEsZCxAyHoxJ65T7ATn4u4SH8akYvowOphVJhTKuHZBN5JPX0rHpWnr9qpnLgNoMYGkPCDn2NNHNkhqDMxJ9rTEtlQQceHDxHvZCfCpLrVL',
         numberId: '119447597909937',
         verifyToken: 'LO_QUE_SEA',
         version: 'v17.0',
     })
+
+*/
+// pruebas
+const adapterProvider = createProvider(MetaProvider, {
+  jwtToken: 'EAAMtNDakSugBAHZBWnT2MEyZCb8SZAmkuEOzic6Km0ZBUzX1pts7JQeMOd4gskpSJRehgWgDxF5HAqU6EBFz7lmO9bWWlQzfUv2esZBAFcuUfU3iJsh4CFq26CA9rkHpfWjeiRqOkp4sIPkmPXs6sCGniYhkgmq2eVAlA8NuWHfsxFagKgLVz',
+  numberId: '102089836052786',
+  verifyToken: 'LO_QUE_SEA',
+  version: 'v17.0',
+})
 
     createBot({
         flow: adapterFlow,
